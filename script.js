d82ee6a2d3da75e89d43b5c3d50b87c48bde3671
jastinesamosamo <jastinesamo@gmail.com>
2026-06-22 12:30:34 +0300

    add register login javascript

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-function salamu() {
-	    alert("Karibu kwenye website yangu!");
+function register(){
+
+	let name = document.getElementById("fname").value;
+
+
+	if(name==""){
+
+		alert("Please enter your name");
+
+	}
+
+	else{
+
+		alert("Registration successful");
+
+
+		document.getElementById("registerBox").style.display="none";
+
+
+		document.getElementById("loginBox").style.display="block";
+
+
+	}
+
+}
+
+
+
+
+function login(){
+
+	alert("Login button clicked");
+
 }

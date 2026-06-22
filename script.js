45aa1db810bdf38764c787d30bc508cdf0a4e033
jastinesamosamo <jastinesamo@gmail.com>
2026-06-22 14:23:54 +0300

    add supabase upload

--- script.js
+++ script.js
@@ -26,7 +26,49 @@ function register(){
 
 const supabaseUrl = "https://yyttzjxjviryjfgwvaof.supabase.co/rest/v1/";
 const supabaseKey = "sb_publishable_Bp5RhJN8D3mkHCIjQB9s0g_5YPKJ2Av";
+const supabaseUrl = "WEKA_API_URL_YAKO";
+const supabaseKey = "WEKA_PUBLIC_KEY_YAKO";
 
+const supabaseClient = supabase.createClient(
+	  supabaseUrl,
+	  supabaseKey
+);
+
+async function uploadFile() {
+	  const name = document.getElementById("name").value;
+	  const file = document.getElementById("file").files[0];
+
+	  if (!file) {
+		      alert("Chagua document kwanza");
+		      return;
+		    }
+
+	  const fileName = Date.now() + "-" + file.name;
+
+	  const { error } = await supabaseClient.storage
+	    .from("documents")
+	    .upload(fileName, file);
+
+	  if (error) {
+		      alert(error.message);
+		      return;
+		    }
+
+	  const { data } = supabaseClient.storage
+	    .from("documents")
+	    .getPublicUrl(fileName);
+
+	  await supabaseClient
+	    .from("documents")
+	    .insert([
+		          {
+				          name: name,
+				          file_url: data.publicUrl
+				        }
+		        ]);
+
+	  alert("Document imepakiwa!");
+}
 
 function login(){
 

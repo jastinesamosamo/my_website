9e2e53b2676a01874fe1937919452fda8db1c0e4
jastinesamosamo <jastinesamo@gmail.com>
2026-06-23 16:19:20 +0300

    Connect form with Supabase

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+
+const supabaseUrl = "https://epqmynznltdljtldkbii.supabase.co/rest/v1/";
+const supabaseKey = "sb_publishable_BhbPXNF1b_axloycjch5Hw_RHT3GSaa";
+
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+async function registerUser() {
+
+	  
+const firstName = document.getElementById("name").value;
+const secondName = document.getElementById("name2").value;
+const thirdName = document.getElementById("name3").value;
+
+const name = firstName + " " + secondName + " " + thirdName;
+
+	  const phone = document.getElementById("phone").value;
+	  const email = document.getElementById("email").value;
+	  const gender = document.getElementById("gender").value;
+	  const birthday = document.getElementById("birthday").value;
+	  const age = document.getElementById("age").value;
+	  const education = document.getElementById("education").value;
+
+	  const { data, error } = await supabaseClient
+	    .from("users")
+	    .insert([
+		          {
+				          name: name,
+				          phone: phone,
+				          email: email,
+				          gender: gender,
+				          birthday: birthday,
+				          age: age,
+				          education: education
+				        }
+		        ]);
+
+	  if (error) {
+		      alert("Imeshindikana: " + error.message);
+		    } else {
+			        alert("Usajili umefanikiwa!");
+			      }
+
+}
+
+
+
 function showLogin() {
     document.getElementById("registerSection").classList.add("hidden");
     document.getElementById("loginSection").classList.remove("hidden");
